dc41bf3b391344d9e2dd39561b097afdfa9cc93c
ehesp <elliot.hesp@gmail.com>
2021-09-03 14:24:48 +0100

    fix: order reviews by creation desc

--- src/hooks/useReviews.ts
+++ src/hooks/useReviews.ts
@@ -1,11 +1,14 @@
-import { doc, query } from 'firebase/firestore';
+import { doc, orderBy, query, QueryConstraint } from 'firebase/firestore';
 import { useFirestoreCollectionData, useFirestoreDoc } from 'reactfire';
 import { collections } from '../firebase';
 
 export function useReviews(productId: string) {
   const collection = collections.productReviews(productId);
 
-  return useFirestoreCollectionData(query(collection));
+  const constraints: QueryConstraint[] = [];
+  constraints.push(orderBy('created_at', 'desc'));
+
+  return useFirestoreCollectionData(query(collection, ...constraints));
 }
 
 export function useReview(productId: string, reviewId: string) {
