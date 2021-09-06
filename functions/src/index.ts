096a65d2b8f3aa857c1d4179c4bcbbea4a3ae448
dackers86 <ackers86@hotmail.com>
2021-09-06 14:26:53 +0100

    removed fucntions gitignore to the root. Removed comments

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,9 +3,6 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 admin.initializeApp();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
 exports.onAuthCreate = functions.auth.user().onCreate(async (user: any) => {
   const collection = admin.firestore().collection('mail');
   await collection.add({ to: user.email, template: { name: 'welcome_email' } });
