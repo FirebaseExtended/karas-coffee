66759c0f64b58e639467b74e0c685ad679bf2c8b
dackers86 <ackers86@hotmail.com>
2021-09-06 14:23:19 +0100

    updated function name

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,7 +6,7 @@ admin.initializeApp();
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-exports.signUp = functions.auth.user().onCreate(async (user: any) => {
+exports.onAuthCreate = functions.auth.user().onCreate(async (user: any) => {
   const collection = admin.firestore().collection('mail');
   await collection.add({ to: user.email, template: { name: 'welcome_email' } });
 
