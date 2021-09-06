80624ff695208080a5bcb4ddcfe33d3fae0b209f
dackers86 <ackers86@hotmail.com>
2021-09-06 17:00:05 +0100

    Removed obsolte promise

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,6 +6,4 @@ admin.initializeApp();
 exports.onAuthCreate = functions.auth.user().onCreate(async (user: any) => {
   const collection = admin.firestore().collection('mail');
   await collection.add({ to: user.email, template: { name: 'welcome_email' } });
-
-  return Promise.resolve();
 });
