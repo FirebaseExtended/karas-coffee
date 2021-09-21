48de1b5b1337a9a094d87486321d9d3587ae4b37
ehesp <elliot.hesp@gmail.com>
2021-09-21 13:19:32 +0100

    feat: use auth subscription

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,27 +1,10 @@
-import { useEffect } from 'react';
-import { onAuthStateChanged, User } from 'firebase/auth';
+import { useAuthUser } from '@react-query-firebase/auth';
 
 import { auth } from '../firebase';
-import { getUserOnce } from '../firebase/auth';
-import { useQuery, useQueryClient } from 'react-query';
 
 /**
  * Subscribes to the current user.
  */
 export function useUser() {
-  const client = useQueryClient();
-
-  useEffect(() => {
-    return onAuthStateChanged(auth, (user) => {
-      const exists = client.getQueryData('user');
-
-      if (exists && !user) {
-        client.setQueryData<User | null>('user', null);
-      } else if (!exists && !!user) {
-        client.setQueryData<User | null>('user', user);
-      }
-    });
-  }, []);
-
-  return useQuery<User | null>('user', getUserOnce);
+  return useAuthUser('user', auth);
 }
