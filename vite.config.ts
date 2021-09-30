f66a736998ee34b3a367bcd1ca28dbfb5b8c80da
ehesp <elliot.hesp@gmail.com>
2021-09-30 15:44:25 +0100

    chore: dependency upgrades

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [react()],
   server: {
     proxy: {
       // Enables the same proxy as Firebase Hosting creates, but for local development.
