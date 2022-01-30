26830edbb790fa8f3a07c47a7d425fe9d335b837
dackers86 <ackers86@hotmail.com>
2022-01-30 16:44:51 +0000

    chore: filtered orders without items

--- website/src/routes/Account/Orders.tsx
+++ website/src/routes/Account/Orders.tsx
@@ -37,14 +37,16 @@ export function Orders() {
     }));
   }
 
+  console.log(orders);
+
   return (
     <section>
-      <h1 className="font-bold text-2xl mb-6">Your Orders</h1>
+      <h1 className="mb-6 text-2xl font-bold">Your Orders</h1>
       <Accordion>
         {orders.isLoading &&
           emptyArray(5).map((_, i) => (
-            <div key={i} className="p-6 flex items-center">
-              <Skeleton className="w-10 h-10 rounded-full mr-2" />
+            <div key={i} className="flex items-center p-6">
+              <Skeleton className="w-10 h-10 mr-2 rounded-full" />
               <div className="flex-grow">
                 <Skeleton className="w-24 h-3" />
                 <Skeleton className="w-64 h-3 mt-2" />
@@ -55,78 +57,81 @@ export function Orders() {
             </div>
           ))}
         {orders.isSuccess &&
-          orders.data.map((order: DocumentData) => (
-            <AccordionItem
-              key={order.id}
-              isOpen={open[order.id]}
-              onToggle={() => toggleOrder(order.id)}
-              collapsible={
-                <>
-                  <div className="flex pb-3">
-                    <div className="flex-grow">
-                      {!!order.charges?.data?.[0].receipt_url && (
-                        <a
-                          href={order.charges.data[0].receipt_url}
-                          target="_blank"
-                          className="text-indigo-600 hover:underline text-sm"
-                          rel="noreferrer"
-                        >
-                          View Receipt &rarr;
-                        </a>
-                      )}
-                    </div>
-                    <div className="font-medium">Sub Total</div>
-                  </div>
-                  <div className="divide-y">
-                    {order.items.map((item: Record<string, never>) => (
-                      <div key={item.id} className="flex items-center py-3">
-                        <div className="flex-grow">
-                          <h3 className="font-bold text-lg">{item.description}</h3>
-                          <div className="text-sm text-gray-600">Quantity: {item.quantity}</div>
-                        </div>
-                        <div className="flex justify-end">
-                          <div className="font-medium">${(item.amount_subtotal / 100).toFixed(2)}</div>
-                        </div>
+          orders.data.map((order: DocumentData) => {
+            if (!order.items) return null; // error with order item
+            return (
+              <AccordionItem
+                key={order.id}
+                isOpen={open[order.id]}
+                onToggle={() => toggleOrder(order.id)}
+                collapsible={
+                  <>
+                    <div className="flex pb-3">
+                      <div className="flex-grow">
+                        {!!order.charges?.data?.[0].receipt_url && (
+                          <a
+                            href={order.charges.data[0].receipt_url}
+                            target="_blank"
+                            className="text-sm text-indigo-600 hover:underline"
+                            rel="noreferrer"
+                          >
+                            View Receipt &rarr;
+                          </a>
+                        )}
                       </div>
-                    ))}
-                    <div className="flex py-3 font-bold">
-                      <div className="flex-grow">Total</div>
-                      <div>${(order.amount / 100).toFixed(2)}</div>
+                      <div className="font-medium">Sub Total</div>
                     </div>
-                  </div>
-                </>
-              }
-            >
-              <div className="flex-grow flex items-center">
-                <div className="flex-grow">
-                  <div className="font-bold">{format(new Date(order.created * 1000), 'eeee, do LLLL, y')}</div>
-                  {!!order.shipping && (
-                    <div className="text-sm text-gray-600">
-                      {[
-                        order.shipping.name,
-                        order.shipping.address.line1,
-                        order.shipping.address.line2,
-                        order.shipping.address.state,
-                        order.shipping.address.city,
-                        order.shipping.address.postal_code,
-                      ]
-                        .filter(Boolean)
-                        .join(', ')}
+                    <div className="divide-y">
+                      {order.items.map((item: Record<string, never>) => (
+                        <div key={item.id} className="flex items-center py-3">
+                          <div className="flex-grow">
+                            <h3 className="text-lg font-bold">{item.description}</h3>
+                            <div className="text-sm text-gray-600">Quantity: {item.quantity}</div>
+                          </div>
+                          <div className="flex justify-end">
+                            <div className="font-medium">${(item.amount_subtotal / 100).toFixed(2)}</div>
+                          </div>
+                        </div>
+                      ))}
+                      <div className="flex py-3 font-bold">
+                        <div className="flex-grow">Total</div>
+                        <div>${(order.amount / 100).toFixed(2)}</div>
+                      </div>
                     </div>
-                  )}
+                  </>
+                }
+              >
+                <div className="flex items-center flex-grow">
+                  <div className="flex-grow">
+                    <div className="font-bold">{format(new Date(order.created * 1000), 'eeee, do LLLL, y')}</div>
+                    {!!order.shipping && (
+                      <div className="text-sm text-gray-600">
+                        {[
+                          order.shipping.name,
+                          order.shipping.address.line1,
+                          order.shipping.address.line2,
+                          order.shipping.address.state,
+                          order.shipping.address.city,
+                          order.shipping.address.postal_code,
+                        ]
+                          .filter(Boolean)
+                          .join(', ')}
+                      </div>
+                    )}
 
-                  {!!order.shippingLabel?.trackingNumber && (
-                    <div className="text-xs text-gray-600">
-                      <span className="font-bold">Tracking Number:</span> {order.shippingLabel.trackingNumber}
-                    </div>
-                  )}
+                    {!!order.shippingLabel?.trackingNumber && (
+                      <div className="text-xs text-gray-600">
+                        <span className="font-bold">Tracking Number:</span> {order.shippingLabel.trackingNumber}
+                      </div>
+                    )}
+                  </div>
+                  <div>
+                    <div className="text-lg font-bold">${(order.amount / 100).toFixed(2)}</div>
+                  </div>
                 </div>
-                <div>
-                  <div className="font-bold text-lg">${(order.amount / 100).toFixed(2)}</div>
-                </div>
-              </div>
-            </AccordionItem>
-          ))}
+              </AccordionItem>
+            );
+          })}
       </Accordion>
     </section>
   );
